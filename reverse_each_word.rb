48533fa21f5fb9a5acf1dfb5cb971b894efa1859
Sofia Jonsson <jonssonsofia93@gmail.com>
2019-02-06 20:13:30 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,11 @@
 def reverse_each_word(string)
  
   sentence1 = string.split(" ")
-  return_array = []
+  array1 = []
   sentence1.each do|string|
-    return_array << string.reverse
+    array1 << string.reverse
   end
-  return_array.join(" ")
+  array1.join(" ")
 end
 
 def reverse_each_word(string)
