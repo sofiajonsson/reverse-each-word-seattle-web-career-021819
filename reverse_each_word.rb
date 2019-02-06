2b839a5991e9adc294e486a60977f018fd8f5312
Sofia Jonsson <jonssonsofia93@gmail.com>
2019-02-06 20:14:13 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(string)
  
   sentence1 = string.split(" ")
   array1 = []
-  sentence1.each do|string|
+  sentence1.each do |string|
     array1 << string.reverse
   end
   array1.join(" ")
@@ -11,7 +11,7 @@ end
 def reverse_each_word(string)
   array = string.split(" ") 
   sentence2 = []
-  array.collect do|string|
+  array.collect do |string|
     sentence2 << string.reverse 
   end
   sentence2.join(" ")
